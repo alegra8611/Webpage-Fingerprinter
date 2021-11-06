48901945970939bdf734246378beab84f51d51de
alegra8611 <76691298+alegra8611@users.noreply.github.com>
2021-11-06 16:00:42 +1000

    Delete js directory

--- js/repeat.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("repeat").onclick = 
-function () {
-	var classes = $(this).parent().attr("class");
-	$(this).parent().attr("class", "animate");
-	var indicator = $(this);
-	setTimeout(function () {
-		$(indicator).parent().addClass(classes);
-		}, 20);
-};	
