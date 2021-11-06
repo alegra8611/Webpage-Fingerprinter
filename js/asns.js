38bc6d3bf0f76f2941c9a11e69e126c8520aa15b
alegra8611 <76691298+alegra8611@users.noreply.github.com>
2021-11-06 16:05:51 +1000

    Delete asns.js

--- js/asns.js
+++
@@ -1,9 +0,0 @@
-$('form').submit(function() {
-     $('input, select, textarea').each(function() {
-       var value = $(this).val(),
-           name = $(this).attr('name');
-       localStorage[name] = value;
-         
-         console.log('stored key: '+name+' stored value: '+value);
-    });   
-});
