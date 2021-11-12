595519e7b01bd6173d6fc1dc41b3125b7dc9accb
alegra8611 <76691298+alegra8611@users.noreply.github.com>
2021-11-13 02:47:08 +1000

    minor typo

--- dashboard.php
+++ dashboard.php
@@ -32,8 +32,8 @@
 
 <?php
    require(dirname(__FILE__) . '/php/connect.php');
-   //$ip = gethostbyname($_SERVER["REMOTE_ADDR"]);
-   $ip = '1.1.0.255';
+   $ip = gethostbyname($_SERVER["REMOTE_ADDR"]);
+   //$ip = '1.1.0.255';
    $sql = "SELECT * FROM geoip WHERE INET_ATON('$ip') BETWEEN ip_from AND ip_to";
    $locstat = mysqli_query($conn,$sql);
    $result = mysqli_query($conn,$sql);
